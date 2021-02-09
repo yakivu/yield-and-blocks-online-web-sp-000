fa5a4dd8e0a11f4608fd90108b9e166e83ee2f71
Adam Walker <adamw207@yahoo.com>
2021-02-09 14:40:50 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
+#def hello_t
 
-end
+#end
 
 # call your method here!
 
+def hello_t(array)
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
+end
+
+# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -4,26 +4,26 @@ describe "#hello_t" do
   let (:names) { ["Tim", "Tom", "Jim"] }
 
   it "calls the block once for each element in the passed-in array" do
-    expect{ hello_t(names){ |name| puts name } }
+    expect { hello_t(names){ |name| puts name } }
       .to output("Tim\nTom\nJim\n")
       .to_stdout
   end
 
   it "returns the original array" do
-    expect( hello_t(names){ |name| puts name } )
-      .to eq(names)
+    expect '( hello_t(names){ |name| puts name } )
+      .to eq(names)'
   end
 
   it "is not hard-coded" do
     other_names = ["Ali", "Jasmine", "Persephone"]
 
-    expect{ hello_t(other_names){ |name| puts name.upcase } }
-      .to output("ALI\nJASMINE\nPERSEPHONE\n")
+    expect '{ hello_t(other_names){ |name| puts name.upcase } }
+      .to output("ALI\nJASMINE\nPERSEPHONE\n")'
       .to_stdout
   end
 
   it "fails gracefully when a block is not passed in" do
-    expect{ hello_t(names) }
+    expect { hello_t(names) }
       .to output("Hey! No block was given!\n")
       .to_stdout
   end
